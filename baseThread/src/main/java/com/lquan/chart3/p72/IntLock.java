8626f9451e23962042c07016f0f336c851f847e2
1570599338@qq.com <1570599338@qq.com>
2019-01-15 18:10:00 +0800

    重入锁

---
+++ baseThread/src/main/java/com/lquan/chart3/p72/IntLock.java
@@ -0,0 +1,28 @@
+package main.java.com.lquan.chart3.p72;
+
+/**
+ * ClassName IntLock
+ *
+ * @Author lquan
+ * @Description // 中断响应
+ * @Date
+ * @Param
+ * @return
+ **/
+public class IntLock implements  Runnable{
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+    }
+}
