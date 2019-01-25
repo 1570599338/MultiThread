b2b89714c7209852ef419e043b86cb11b21bd38f
1570599338@qq.com <1570599338@qq.com>
2019-01-25 10:01:36 +0800

    锁的非限时等待

--- baseThread/src/main/java/com/lquan/chart3/p74/Trylock.java
+++ baseThread/src/main/java/com/lquan/chart3/p74/Trylock.java
@@ -35,68 +35,81 @@ public class Trylock implements Runnable {
      */
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName()+":进入");
+        System.out.println(Thread.currentThread().getName()+":准备");
         if(lock==1){
             while(true){
-
+                System.out.println(Thread.currentThread().getName()+":进入-1");
                 if(lock1.tryLock()){
                     try {
                         try {
-                            System.out.println(Thread.currentThread().getName()+":获取锁1");
+                            System.out.println(Thread.currentThread().getName()+":获取锁1-1");
                             Thread.sleep(500);
                         } catch (InterruptedException e) { }
 
                         if(lock2.tryLock()){
                             try {
-                                System.out.println(Thread.currentThread().getName()+":获取锁2");
+                                System.out.println(Thread.currentThread().getName()+":获取锁2-return-1");
                                 return;
                             } finally {
                                 lock2.unlock();
-                                System.out.println(Thread.currentThread().getName()+"放弃锁2");
+                                System.out.println(Thread.currentThread().getName()+"结束释放锁2-1");
                             }
+                        }else{
+                            System.out.println(Thread.currentThread().getName()+"获取锁2失败-1");
                         }
                     } finally {
                         lock1.unlock();
+                        System.out.println(Thread.currentThread().getName()+":结束释放锁1-1");
                     }
+                }else{
+                    System.out.println(Thread.currentThread().getName()+"获取锁1失败-1");
                 }
 
             }
 
         }else {
             while(true){
+                System.out.println(Thread.currentThread().getName()+":2进入-2");
                 if(lock2.tryLock()){
 
                     try {
                         try {
-                            System.out.println(Thread.currentThread().getName()+":获取锁2");
+                            System.out.println(Thread.currentThread().getName()+":获取锁2-2");
                             Thread.sleep(500);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                         if(lock1.tryLock()){
                             try {
-                                System.out.println(Thread.currentThread().getName()+":获取锁1");
+                                System.out.println(Thread.currentThread().getName()+":获取锁1-return-2");
                                 return;
                             } finally {
                                 lock1.unlock();
-                                System.out.println(Thread.currentThread().getName()+"放弃锁1");
+                                System.out.println(Thread.currentThread().getName()+"结束释放锁1-2");
                             }
+                        }else{
+                            System.out.println(Thread.currentThread().getName()+"获取锁1失败-2");
                         }
                     } finally {
                         lock2.unlock();
+                        System.out.println(Thread.currentThread().getName()+"结束释放锁2-2");
                     }
+                }else{
+                    System.out.println(Thread.currentThread().getName()+"获取锁2失败-2");
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
-        Trylock r1 =new Trylock(1);
-        Trylock r2 =new Trylock(2);
-        Thread t1 = new Thread(r1,"r1-1");
-        Thread t2 = new Thread(r2,"r2-2");
-        t1.start();
-        t2.start();
+    public static void main(String[] args) throws Exception{
+            Trylock r1 =new Trylock(1);
+            Trylock r2 =new Trylock(2);
+            Thread t1 = new Thread(r1,"r1-1");
+            Thread t2 = new Thread(r2,"r2-2");
+            t1.start();
+            t2.start();
+           // t1.join();
+           // t2.join();
     }
 }
 
