7aecf8049d941adc795b851170521ea5b7f093d6
lquan <1570599338@qq.com>
2022-11-09 18:21:26 +0800

    test

---
+++ diagrammatize/src/main/java/com/lquan/thread/Test.java
@@ -0,0 +1,20 @@
+package com.lquan.thread;
+
+/**
+ * @program: MultiThread
+ * @description:
+ * @author: lquan
+ * @create: 2022-11-09 15:53
+ **/
+public class Test{
+
+    public static void main(String[] args){
+        while(true){
+
+            System.out.print("***************"+args[0]);
+        }
+
+
+    }
+
+}
